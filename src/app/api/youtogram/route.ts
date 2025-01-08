fc9ba16db58a0835afd45378aa6ce7987f5d36bb
maziar <maziar.rumiani@gmail.com>
2025-01-08 18:19:38 +0330

    puppeteer-core and sparticuz/chromium added

--- src/app/api/youtogram/route.ts
+++ src/app/api/youtogram/route.ts
@@ -16,6 +16,8 @@ bot.command("start", (ctx) => {
   ctx.reply(`Hi ${ctx.from?.first_name},\nPlease send me a Youtube username.`);
 });
 bot.on("message:text", async (ctx) => {
+  console.log(ctx.message.text);
+  
   const result = await getSubshandler(ctx.message.text);
   await ctx.reply(result!);
 });
--- src/app/components/channel/channel.tsx
+++ src/app/components/channel/channel.tsx
@@ -1,11 +1,23 @@
-import puppeteer from "puppeteer";
+import puppeteer from "puppeteer-core";
+import chromium from "@sparticuz/chromium";
 
 export default async function getSubshandler(username: string) {
+  function formatUsername(username: string) {
+    return username
+      .toLowerCase() // Convert to lowercase
+      .replace(/\s+/g, "_") // Replace spaces with underscores
+      .replace(/[^a-z0-9_]/g, ""); // Remove non-alphanumeric characters except underscores
+  }
+  const newUsername = formatUsername(username);
   try {
-    const browser = await puppeteer.launch();
+    const browser = await puppeteer.launch({
+      executablePath: await chromium.executablePath(), // Use the Chromium path from @sparticuz/chromium
+      headless: true, // Make sure it's in headless mode for serverless
+      args: chromium.args, // Optional: Pass arguments optimized for serverless environments
+    });
     const page = await browser.newPage();
 
-    await page.goto(`https://www.youtube.com/@${username}`);
+    await page.goto(`https://www.youtube.com/@${newUsername}`);
     const subscribersData = await page.evaluate(() => {
       // Find the span element that contains "subscribers"
       const spanElement = Array.from(document.querySelectorAll("span")).find(
@@ -16,7 +28,7 @@ export default async function getSubshandler(username: string) {
     });
     await browser.close();
     return subscribersData
-      ? `${username}: ${subscribersData}`
+      ? `${newUsername}: ${subscribersData}`
       : "Channel not found!";
   } catch (error) {
     console.error(error);
