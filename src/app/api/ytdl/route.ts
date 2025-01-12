4c9513e2d428c618a3ef4e2b5d7720a99a95fea5
maziar <maziar.rumiani@gmail.com>
2025-01-12 19:45:42 +0330

    fixed typo: NextResponse as a parameter is not valid in newer next versions

--- src/app/api/ytdl/route.ts
+++ src/app/api/ytdl/route.ts
@@ -1,17 +1,18 @@
 // import ytdl from "ytdl-core";
 import ytdl from "@distube/ytdl-core";
 
-import { NextApiResponse } from "next";
 import fs from "fs";
+import { NextRequest, NextResponse } from "next/server";
 
-export async function GET(req: Request, res: NextApiResponse) {
+export const GET = async (req: NextRequest) => {
   const videoUrl = "https://www.youtube.com/watch?v=MPL0n5Pd8Js";
+  console.log(req);
 
   try {
     console.log(videoUrl);
     // const info = await ytdl.getInfo(videoUrl);
     const info = await ytdl.getBasicInfo(videoUrl);
-    
+
     console.log(info.formats);
     const writeStream = fs.createWriteStream("./downloaded_video.mp4");
     ytdl(videoUrl, {
@@ -24,9 +25,9 @@ export async function GET(req: Request, res: NextApiResponse) {
     writeStream.on("error", (err) => {
       console.error("Error during download:", err);
     });
-    return Response.json("done");
+    return NextResponse.json("done");
   } catch (error) {
     console.error("Error downloading video:", error);
-    return Response.json("Failed to download video", {});
+    return NextResponse.json("Failed to download video", {});
   }
-}
+};
